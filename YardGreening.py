ba2211144a464f8cf41fc563e37fea8cac7d9cee
dkarabelev <145501324+dkarabelev@users.noreply.github.com>
2023-09-20 16:35:32 +0300

    Delete YardGreening.py

--- YardGreening.py
+++
@@ -1,6 +0,0 @@
-area = float(input())
-total_before_discount = area * 7.61
-discount = total_before_discount * 0.18
-after_discount = total_before_discount - discount
-print(f"The final price is: {after_discount} lv.")
-print(f"The discount is: {discount} lv.")
