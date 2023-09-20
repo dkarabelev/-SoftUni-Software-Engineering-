535667650c874c2a53de0e3d4624daa7812e39a2
dkarabelev <145501324+dkarabelev@users.noreply.github.com>
2023-09-20 16:35:23 +0300

    Delete PetShop.py

--- PetShop.py
+++
@@ -1,6 +0,0 @@
-dogfood_price=int(input())
-catfood_price=int(input())
-total_dogfood=dogfood_price * 2.5
-total_catfood=catfood_price * 4
-total_sum = total_dogfood + total_catfood
-print(f'{total_sum} lv.')
